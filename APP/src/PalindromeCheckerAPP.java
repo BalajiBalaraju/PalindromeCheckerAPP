c99ec82f52df74dd3a24b9b88f52d614c8fe9539
dm5603 <blackbeast412@gmail.com>
2026-02-19 09:56:37 +0530

    Give Proper Commit Message

--- APP/src/PalindromeCheckerAPP.java
+++ APP/src/PalindromeCheckerAPP.java
@@ -1,17 +1,27 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerAPP {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string:");
-        String input = sc.nextLine();
 
-        String reversed = new StringBuilder(input).reverse().toString();
+        // Hardcoded string
+        String input = "madam";
 
-        if(input.equalsIgnoreCase(reversed)) {
-            System.out.println("It is a Palindrome");
+        // Assume it is palindrome initially
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            // Compare characters from both ends
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;   // Stop loop if mismatch found
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
         } else {
-            System.out.println("Not a Palindrome");
+            System.out.println(input + " is NOT a Palindrome.");
         }
     }
 }
